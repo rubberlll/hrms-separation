fbd5377a3f0d6d5b9c803ded64310df10d776c38
rubberlll <2405206056@qq.com>
2025-06-05 16:23:44 +0800

    更改云mgDB后重新build

---
+++ back-end/.next/static/MllCPIi5K2alysqi1SDqo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[{has:void 0,source:"/files/:path*",destination:"/api/files/:path*"}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
